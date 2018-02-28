55732babed63221520f86c22994dd695def5a12b
RomeoE <enache.romeo@gmail.com>
2018-02-28 13:28:58 +0200

    adds localization

---
+++ assets/jquery_i18n/main-jquery_i18n.js
@@ -0,0 +1,17 @@
+// jQuery(document).ready(function() {
+//   var update_texts = function() {
+//     $('body').i18n();
+//   };
+
+//   // $('.lang-switch').click(function(e) {
+//   //   e.preventDefault();
+//   //   $.i18n().locale = $(this).data('locale');
+//   //   update_texts();
+//   // });
+
+  
+
+//   $.i18n({locale: 'ml'});
+
+//   // update_texts();
+// });
